4e218dc8b656c01daf877762a732e7f111072a73
utariq8688 <utariq68@hotmail.com>
2022-05-09 11:37:17 +1000

    initial code

---
+++ src/main/java/wipro/service/MyOperations.java
@@ -0,0 +1,13 @@
+package wipro.service;
+
+public class MyOperations {
+	
+	public long getSummation(int a, int b) {
+		return a + b;
+	}
+	
+	public String getFullName(String firstName, String lastName) {
+		return firstName + " " + lastName;
+	}
+
+}
---
+++ src/test/java/wipro/service/MyOperationsTest.java
@@ -0,0 +1,39 @@
+package wipro.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyOperationsTest {
+
+	@Test
+	public void testGetSummation() {
+		assertEquals(8, new MyOperations().getSummation(3, 5));
+	}
+	
+	@Test
+	public void testGetSummation1() {
+		assertEquals(16, new MyOperations().getSummation(9, 7));
+	}
+	
+	@Test
+	public void testGetSummation2() {
+		assertEquals(51, new MyOperations().getSummation(46, 5));
+	}
+	
+	@Test
+	public void testGetFullName() {
+		assertEquals("Prabhu Jeypal", new MyOperations().getFullName("Prabhu", "Jeypal"));
+	}
+	
+	@Test
+	public void testGetFullName1() {
+		assertEquals(" Jeypal", new MyOperations().getFullName(null, "Jeypal"));
+	}
+	
+	@Test
+	public void testGetFullName2() {
+		assertEquals("INVALID_INPUT", new MyOperations().getFullName("Prabhu", null));
+	}
+
+}
